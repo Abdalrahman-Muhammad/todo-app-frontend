9e87c1a0c61b8f469a11d7073ca33c936da70bbf
Abdalrahman-Muhammad <abdelrhmanmohamed421@gmail.com>
2023-10-31 23:28:59 +0200

    Add: add new types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -8,3 +8,44 @@ interface IAuthenticatedUser {
   name: string;
   email: string;
 }
+
+interface ICategory {
+  id: string;
+  name: string;
+  user: IUser | string;
+  isEditable: boolean;
+  color: IColor;
+  icon: IIcon;
+}
+interface IColor {
+  id: string;
+  name: string;
+  code: string;
+}
+interface IIcon {
+  id: string;
+  name: string;
+  symbol: string;
+}
+
+interface ICategoryRequest {
+  name: string;
+  color: IColor;
+  icon: IIcon;
+}
+
+interface ITask {
+  id: string;
+  name: string;
+  isCompleted: boolean;
+  categoryId: string;
+  createdAt: string;
+  date: string;
+}
+
+interface ITaskRequest {
+  name: string;
+  isCompleted: boolean;
+  categoryId: string;
+  date: string;
+}
