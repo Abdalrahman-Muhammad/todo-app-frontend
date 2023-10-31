e8d1ab87623998185579a706af75830a80d1daa1
Abdalrahman-Muhammad <abdelrhmanmohamed421@gmail.com>
2023-10-31 03:49:48 +0200

    Add: safe-area-wrapper

---
+++ src/components/shared/safe-area-wrapper.tsx
@@ -0,0 +1,20 @@
+import theme from "@/utils/theme";
+import { ReactNode } from "react";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+type SafeAreaWrapperProps = {
+  children: ReactNode;
+};
+
+const SafeAreaWrapper = ({ children }: SafeAreaWrapperProps) => {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.white }}>
+      {children}
+    </SafeAreaView>
+  );
+};
+
+export default SafeAreaWrapper;
+
+const styles = StyleSheet.create({});
