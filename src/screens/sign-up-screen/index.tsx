160484601766f91351d17975955e2d7c79d9d3d5
Abdalrahman-Muhammad <abdelrhmanmohamed421@gmail.com>
2023-11-02 01:10:25 +0200

    Add activity indicator and email validation in signup

--- src/screens/sign-up-screen/index.tsx
+++ src/screens/sign-up-screen/index.tsx
@@ -1,16 +1,24 @@
 import Button from "@/components/shared/Button";
 import { Box, Text } from "@/utils/theme";
 import React from "react";
-import { Pressable, StyleSheet } from "react-native";
+import { Pressable, StyleSheet, ActivityIndicator } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { AuthScreenNavigationType } from "@/navigation/types";
-
+import { useState } from "react";
 import SafeAreaWrapper from "@/components/shared/safe-area-wrapper";
 import Input from "@/components/shared/Input";
 import { Controller, useForm } from "react-hook-form";
 import { registerUser } from "@/services/api";
 
+export const validateEmail = function (email: string): boolean {
+  // Regular expression for email validation
+  const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+  return emailRegex.test(email);
+};
+
 const SignUpScreen = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const navigation = useNavigation<AuthScreenNavigationType<"SignUp">>();
   const navigateTosignInScreen = () => {
     navigation.navigate("SignIn");
@@ -18,6 +26,7 @@ const SignUpScreen = () => {
   const {
     control,
     handleSubmit,
+    setError,
     formState: { errors },
   } = useForm<IUser>({
     defaultValues: {
@@ -28,12 +37,28 @@ const SignUpScreen = () => {
 
   const onSubmit = async (data: IUser) => {
     try {
+      setIsLoading(true);
       const { email, name, password } = data;
+      if (!validateEmail(email)) {
+        console.log("Invalid email format");
+        setError("email", {
+          type: "manual",
+          message: "Invalid email format",
+        });
+        setIsLoading(false);
+        return;
+      }
       // register user
       await registerUser({ email, password, name });
+      setIsLoading(false);
+
       //navigate to login page
       navigateTosignInScreen();
-    } catch (error) {}
+    } catch (error) {
+      setIsLoading(false);
+
+      console.log("🚀 ~ file: index.tsx:42 ~ onSubmit ~ error:", error);
+    }
   };
 
   return (
@@ -107,6 +132,12 @@ const SignUpScreen = () => {
         <Box mb="6" />
 
         <Button label="Register" onPress={handleSubmit(onSubmit)} upperCase />
+        {isLoading ? (
+          <Box style={{ marginTop: 20 }}>
+            <ActivityIndicator size="large" color="#007AFF" />
+            <Text>Loading...</Text>
+          </Box>
+        ) : null}
       </Box>
     </SafeAreaWrapper>
   );
